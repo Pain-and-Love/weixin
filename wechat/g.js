2b6507fa31467a8fa57ec61cad70f86a52593638
XiaFan <506868247@qq.com>
2017-11-30 01:08:12 +0800

    将验证过程封装成一个Koa中间件

---
+++ wechat/g.js
@@ -0,0 +1,20 @@
+var sha1 = require('sha1');
+
+module.exports = function(config) {
+    return function *(next) {
+    console.log('g.js => ', this.query);
+    var token = config.wechat.token;
+    var signature = this.query.signature;
+    var nonce = this.query.nonce;
+    var timestamp = this.query.timestamp;
+    var echostr = this.query.echostr;
+
+    var str = [token, timestamp, nonce].sort().join('');
+    var sha = sha1(str);
+    if (sha === signature) {
+        this.body = echostr + '';
+    } else {
+        this.body = 'Wrong~~';
+    }
+}
+}
