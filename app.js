657634ce38470f1d4d2ae7d570d38631e38a04e7
XiaFan <506868247@qq.com>
2017-11-29 22:05:21 +0800

    与微信对接...

---
+++ app.js
@@ -0,0 +1,31 @@
+var Koa = require('koa');
+var sha1 = require('sha1');
+var config = {
+    wechat: {
+        appID: 'wxeaa02ad3ac62bce7',
+        appSecret: 'f2bd3d8f6931eabe9510a822dd46364f',
+        token: 'painandlove6'
+    }
+};
+
+var app = new Koa();
+
+app.use(function *(next) {
+    console.log(this.query);
+    var token = config.wechat.token;
+    var signature = this.query.signature;
+    var nonce = this.query.nonce;
+    var timestamp = this.query.timestamp;
+    var ecostr = this.query.ecostr;
+
+    var str = [token, timestamp, nonce].sort();
+    var sha = sha1(str);
+    if (sha === signature) {
+        this.body = ecostr + '';
+    } else {
+        this.body = 'Wrong~~';
+    }
+});
+
+app.listen(8080);
+console.log('Listen at port: 8080')
